7ea12c0353e76710238471c4b483215666efc53a
Victor H <wd.victorhugo@gmail.com>
2019-09-06 00:54:04 -0300

    feat(#20)/Working with nodejs

---
+++ JS cheat sheet/Creating Node app/first-app/server.js
@@ -0,0 +1,6 @@
+const express = require('express'); // import express module
+
+const app = express(); //instantiate express function
+
+app.listen(8888); //up a server on port 8888
+                    //type localhost:8888 in the navigator to acess
